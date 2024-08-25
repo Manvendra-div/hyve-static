4883b7c627b426cca50951be8b64e551972bafd1
Manvendra Singh <ms1577239@gmail.com>
2024-08-25 16:02:29 +0530

    some upgrades

--- animations.js
+++ animations.js
@@ -1,10 +1,12 @@
 const serviceContainer = document.getElementById("service-container");
 const dropdown = document.getElementById("service-dropdown");
-
+const mobileNav = document.getElementById("mobile-nav-btn");
+const mobileNavPanel = document.getElementById("mobile-nav-panel");
 function showDropdown() {
   gsap.to(dropdown, {
+    display:"flex",
     opacity: 1,
-    zIndex:999,
+    zIndex: 999,
     duration: 0.5,
     ease: "power3.out",
   });
@@ -12,15 +14,42 @@ function showDropdown() {
 
 function hideDropdown() {
   gsap.to(dropdown, {
+    display: "none",
     opacity: 0,
     zIndex: -1,
     duration: 0.5,
     ease: "power3.out",
   });
 }
-//  
-// Show dropdown when mouse enters the container
-serviceContainer.addEventListener("mouseenter", showDropdown);
 
-// Hide dropdown when mouse leaves the container
+function showMobileNav() {
+  gsap.to(mobileNavPanel, {
+    left: 0,
+    zIndex: 999,
+    duration: 0.5,
+    ease: "power3.out",
+  });
+  document.body.style.overflow = "hidden";
+  document.body.style.position = "fixed";
+  document.body.style.width = "100%";
+  mobileNav.removeEventListener("click", showMobileNav);
+  mobileNav.addEventListener("click", closeMobileNav);
+}
+
+function closeMobileNav() {
+  gsap.to(mobileNavPanel, {
+    left: "-100%",
+    zIndex: 0,
+    duration: 0.5,
+    ease: "power3.out",
+  });
+  document.body.style.overflow = "auto";
+  document.body.style.position = "";
+  document.body.style.width = "";
+  mobileNav.removeEventListener("click", closeMobileNav);
+  mobileNav.addEventListener("click", showMobileNav);
+}
+
+serviceContainer.addEventListener("mouseenter", showDropdown);
 serviceContainer.addEventListener("mouseleave", hideDropdown);
+mobileNav.addEventListener("click", showMobileNav);
